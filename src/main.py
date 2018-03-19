22403ed929eaee809d9210ff7ce6858911d128db
Felix Bouleau <bouleau@spotify.com>
2018-03-19 22:12:42 +0000

    work

--- src/main.py
+++ src/main.py
@@ -11,10 +11,13 @@ import time
 
 # 	Take picture with synced settings
 
+
 def triggered_callback(channel):
     print('This is a edge event callback function!')
     print('Edge detected on channel %s'%channel)
     print('This is run in a different thread to your main program')
+    # TODO send static mock picture so we can work on the gif creation and
+    # serving from master node
     r = requests.get('http://192.168.1.123/sync')
 
 #	Upload picture
@@ -22,9 +25,9 @@ def triggered_callback(channel):
 # 	Re-sync settings
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=80)
  	GPIO.setmode(GPIO.BCM)
  	channel = 20
  	GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
  	while True:
  		GPIO.add_event_detect(channel, GPIO.RISING, callback=triggered_callback, bouncetime=200)
+ 		GPIO.remove_event_detect(channel)
