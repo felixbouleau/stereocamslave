24371d93dec25427687bb747305ccbcae3ce675a
Felix Bouleau <bouleau@spotify.com>
2018-03-19 21:35:07 +0000

    work

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 from flask import Flask
+import requests
+import time
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+# TODO
+
+# Regularly sync settings from masters /sync endpoint
+
+# Wait for IO input
+
+# 	Take picture with synced settings
+
+#	Upload picture
+
+# 	Re-sync settings
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)
+ 	while True:
+ 		r = requests.get('http://192.168.1.123/sync')
+ 		time.sleep(30)
