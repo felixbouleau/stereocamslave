213c1d910e7f4d65278ba56a14b392e5e2ae6285
Felix Bouleau <bouleau@spotify.com>
2018-04-23 20:32:57 +0000

    fix picamera stuff

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import time
 import RPi.GPIO as GPIO
 import time
 import os
+import picamera
 
 SLAVE_ID = None
 
@@ -14,6 +15,13 @@ SLAVE_ID = None
 
 # 	Take picture with synced settings
 
+def take_picture():
+	camera = PiCamera()
+	camera.resolution = (1024, 768)
+	camera.start_preview()
+	# Camera warm-up time
+	sleep(2)
+	camera.capture('/data/foo.jpg')
 
 def triggered_callback(channel):
     print('Trigger detected on channel %s. Uploading sample image...'%channel)
@@ -33,6 +41,8 @@ def triggered_callback(channel):
 
 if __name__ == '__main__':
  	
+	take_picture()
+	
 	# Get slave ID ("which camera in the sequence am I?")
 	SLAVE_ID = os.environ["SLAVE_ID"]
 	if SLAVE_ID is None:
